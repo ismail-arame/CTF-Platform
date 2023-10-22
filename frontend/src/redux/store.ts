a81ff344f29873605454ad5a14cf0c4c4ca0cca8
ismail-arame <ismail.arame66@gmail.com>
2023-10-22 10:30:22 +0100

    added redux persist to persist the user slice

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,14 +1,59 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import createFilter from "redux-persist-transform-filter";
+
+//Slices
 import userSlice from "./features/userSlice";
+
+// Storage
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key: any) {
+      return Promise.resolve(null);
+    },
+    setItem(_key: any, value: any) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key: any) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage =
+  typeof window !== "undefined"
+    ? createWebStorage("local")
+    : createNoopStorage();
+
+// saveUserOnlyFilter
+const saveUserOnlyFilter = createFilter("user", ["user"]);
+
+// Persist Config
+const persistConfig = {
+  key: "user",
+  storage: storage,
+  whitelist: ["user"],
+  transforms: [saveUserOnlyFilter],
+};
 
 const rootReducer = combineReducers({
   user: userSlice,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
